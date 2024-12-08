53b01213c6c6dcafdbf2eccaf7e1c405137176b7
2042458785 <2042458785@qq.com>
2024-12-08 16:49:21 +0800

    my first solidity commit

---
+++ mytest111/mynewfile.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+//指定了智能合约的版本
+//comment: this is my first  smart contract
+
+contract Helloworld{
+    string readvalue="Hello world";
+    function sayhello() public view returns (string memory ){
+        return Addinfo(readvalue);
+    }
+    string RTTT1="reset ok!";
+    // test:
+    // string RT2="reset ok2!";
+    function Resethello(string memory Mynewresetstring) public returns (string memory RT){
+        readvalue=Mynewresetstring;
+        return  RTTT1;
+    }
+    string ret="";
+    function Addinfo(string memory Hellost) internal pure returns(string memory outstr){
+        return string.concat(Hellost,"\tthis is add string!");
+    }
+}

53239148288838d466b3b17fa73deb3e27f53d87
2042458785 <2042458785@qq.com>
2025-10-20 15:19:41 +0800

    10.20 afternoon

--- NewFundMe/FundTokenERC20.sol
+++ NewFundMe/FundTokenERC20.sol
@@ -1,12 +1,18 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
+// FundMe
+//1.让FundMe的参与者,基于mapping来领取相应数量的通证
+//2.让FundMe的参与者,transfer通证
+//3.在使用完成后,burn通证
 
 import {ERC20} from   "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {FundMe} from "./fundme.sol";
+
 
 contract FundMeERC20 is ERC20 {
-    constructor() ERC20("FundMeToken","FDFT"){
-
+    FundMe fundme;
+    constructor(address fundMeddr) ERC20("FundMeToken","FDFT"){
+        fundme=FundMe(fundMeddr);
     }
 }

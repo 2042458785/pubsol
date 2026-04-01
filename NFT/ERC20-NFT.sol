c602e45f97a8526773629a075bac94bf3f67a0db
sudo-ddy <2042458785@qq.com>
2026-04-01 14:57:33 +0800

    可自定义NFT使用默认NFT 可在ETH mainnet 上查看

---
+++ NFT/ERC20-NFT.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.6.0
+pragma solidity ^0.8.34;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract Drunk is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
+    uint256 private _nextTokenId;
+
+    string constant DefaultMetaData="https://ipfs.io/ipfs/bafkreibo2wdolf54wpa7uc5lujz7dnzcumbk4baejn36j3mcjv3ge7l7bm";
+    string private MetaData;
+
+    constructor(address initialOwner)
+        ERC721("Drunk", "DRK")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to,string memory InMetaData)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        if (keccak256(abi.encodePacked(InMetaData)) == keccak256(abi.encodePacked("default"))){ //判断字符串是否相等
+            MetaData=DefaultMetaData; //使用default的MetaData
+        }
+        //不是default,使用传入的MetaData
+        MetaData=InMetaData;
+        _setTokenURI(tokenId,MetaData);
+        return tokenId;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
